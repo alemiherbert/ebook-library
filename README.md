4451ed7ab8c5566d0cb5211b402887bbf5024c77
Alemi Herbert <alemiherbert@gmail.com>
2024-06-06 11:53:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ebook Library
+A very simple e-book store built as one of the projects for The Odin Project.
---
+++ main.js
@@ -0,0 +1,82 @@
+#!/usr/bin/node
+
+const bookListContent = document.getElementById('book-list-content');
+const modal = document.getElementById('enter-book');
+const closeModal = document.getElementById('cancel');
+const form = document.getElementById('book-entry-form');
+
+function scrollContentLeft() {
+    bookListContent.scrollBy(-72, 0);
+}
+
+function scrollContentRight() {
+    bookListContent.scrollBy(72, 0);
+}
+
+function addBook() {
+    modal.style.display = 'block';
+}
+
+closeModal.onclick = function() {
+    modal.style.display = 'none';
+    form.reset();
+}
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+}
+
+class Library {
+    constructor() {
+        this.books = [];
+    }
+
+    addBook(book) {
+        this.books.push(book);
+    }
+
+    printBooks() {
+        bookListContent.innerHTML = '';
+        for (let book of this.books) {
+            const output = `
+                <div class="book">
+                    <p>${book.title}</p>
+                    <p>${book.author}</p>
+                    <p>${book.pages} Pages</p>
+                    <p>${book.read ? 'Read' : 'Not Read'}</p>
+                </div>`;
+            bookListContent.innerHTML += output;
+        }
+        bookListContent.innerHTML += `
+            <div class="book" id="add-book">
+                <button onclick="addBook()">+</button>
+            </div>`;
+    }
+}
+
+const library = new Library();
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const book = new Book(
+        document.getElementById('book-title').value,
+        document.getElementById('book-author').value,
+        document.getElementById('book-pages').value,
+        true
+    );
+    library.addBook(book);
+    form.reset();
+    library.printBooks();
+    modal.style.display = 'none';
+});
+
+const defaultBook = new Book('The Hobbit', 'J.R.R Tolkien', 295, true);
+const defaultBook2 = new Book('1984', 'George Orwell', 252, true);
+library.addBook(defaultBook);
+library.addBook(defaultBook2);
+library.printBooks();
